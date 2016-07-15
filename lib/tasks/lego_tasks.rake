e10f88a4ab832667aae45f19d0981c0232a4052a
bryanelidimas <bryanelidimas@gmail.com>
2016-07-15 18:41:20 -0500

    rails new lego --full -d postgresql --skip-turbolinks

--- lib/tasks/lego_tasks.rake
+++
@@ -1,4 +0,0 @@
-# desc "Explaining what the task does"
-# task :lego do
-#   # Task goes here
-# end
--- test/lego_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class LegoTest < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, Lego
-  end
-end
